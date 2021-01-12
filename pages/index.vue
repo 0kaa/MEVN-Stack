5837d46766ede97e2cba8a9ed8fddc4a59b89ad0
mahmoud sabry <thebestoka0@gmail.com>
2021-01-12 06:14:43 +0200

    some changes

--- pages/index.vue
+++ pages/index.vue
@@ -70,6 +70,7 @@ export default {
           .then(res => {
             console.log(res.data);
             this.$refs.form.reset();
+            this.dialog = false
           })
           .catch(err => {
             console.log(err);
