42f0fec046b0fe32e356fd1c3dd5254b4a207469
mahmoud sabry <thebestoka0@gmail.com>
2021-04-15 19:04:24 +0200

    procfile

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,8 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    titleTemplate: "%s - express-nuxt",
-    title: "express-nuxt",
+    titleTemplate: "Hatlaqini",
+    title: "Hatlaqini",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
